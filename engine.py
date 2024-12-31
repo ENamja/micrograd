c7d5f85bc1c54f368537e96e3199ea6b02afd160
elisha <namelisha@gmail.com>
2024-12-30 17:09:49 -0800

    Implement loss function and show an example of gradient descent for a sample input and target outputs

--- engine.py
+++ engine.py
@@ -1,6 +1,5 @@
 import math
 import random
-
 class Value:
 
     def __init__(self, data, _children=(), _op=''):
@@ -108,6 +107,9 @@ class Neuron:
         out = activation.tanh()
         return out
     
+    def parameters(self):
+        return self.w + [self.b]
+    
 class Layer:
 
     def __init__(self, nin, nout):
@@ -116,6 +118,9 @@ class Layer:
     def __call__(self, x):
         outs = [n(x) for n in self.neurons]
         return outs[0] if len(outs) == 1 else outs
+    
+    def parameters(self):
+        return [p for neuron in self.neurons for p in neuron.parameters()]
     
 class MLP:
 
@@ -127,6 +132,9 @@ class MLP:
         for layer in self.layers:
             x = layer(x)
         return x
+    
+    def parameters(self):
+        return [p for layer in self.layers for p in layer.parameters()]
 
 ## Example:
 
@@ -139,5 +147,26 @@ inputs = [
 outputs = [1.0, -1.0, -1.0, 1.0] # desired targets
 
 n = MLP(3, [4, 4, 1]) # Create a Multi-Layer Perceptron that takes in 3 inputs, has 2 hidden layers of size 4, and a single output
-ypred = [n(x) for x in inputs]
-print(ypred) # Completely off so far due to random weights and no use of a loss function (literally just random guesses pretty much right now)
+
+preds = None
+
+loss = Value(1000)
+
+# Gradient Descent
+while loss.data > 0.001:
+    # forward pass
+    preds = [n(x) for x in inputs]
+    loss = sum((yout - ygt)**2 for ygt, yout in zip(outputs, preds))
+
+    # backward pass
+    for p in n.parameters():
+        p.grad = 0.0 # Reset gradients for weights to 0 after every adjustment
+    loss.backward()
+
+    # update
+    for p in n.parameters():
+        p.data += -0.05 * p.grad
+
+    print(loss.data)
+
+print(preds)
